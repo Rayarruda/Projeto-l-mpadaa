b28a4f975fffb8560c574db3d22d6aef0deeecf2
Ray <rayarruda0309@gmail.com>
2026-01-09 15:58:02 -0300

    update main.js

--- main.js
+++ main.js
@@ -4,25 +4,58 @@ var vermelho = "vermelho.png";
 var azul = "azul.png";
 var quebar = "quebrada.png";
 
+
+let estado = "desligado"
+
 function Ligarr() {
- document.getElementById("apagada").src = ligar;
+ 
+ if (estado === "quebrado") {
+  alert("Você não pode ligar uma lâmpada quebrada, troque antes!");
+  estado = "quebrado";
+  
+ } else if (estado === "Mudado") {
+  alert("A lâmpada ja está acesa, mas com outra cor!")
+  
+ } else {
+  document.getElementById("apagada").src = ligar;
+  estado = "ligar";
+ }
+ 
 }
 
 function Desligar() {
+ if (estado === "quebrado") {
+  alert("Você não pode desligar uma lâmpada quebrada, troque antes!");
+  estado = "quebrado";
+ } else {
  document.getElementById("apagada").src = desligar;
-}
+ estado = "desligado"
+}}
 
 function Trocar() {
- document.getElementById("apagada").src = desligar;
+ document.getElementById("apagada").src =
+ desligar;
+ estado = "trocado"
 }
 
 function Cor() {
+ 
+ if (estado === "desligado") {
+  alert("Você precisa ligar antes!")
+  
+ } else if (estado === "quebrado") {
+  alert("Luz quebrada, troque antes!")
+  
+ } else {
  document.getElementById("apagada").src = azul;
  let aux = vermelho 
  vermelho = azul
  azul = aux
+ estado = "Mudado"
+ }
 }
 
 function Quebrar () {
  document.getElementById("apagada").src = quebar;
+ estado = "quebrado";
 }
